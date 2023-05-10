152b8cf1ad926e4b39afcb187ce8f7373cb18618
Mihle Tshoko <mihletshoko@protonmail.com>
2023-05-10 05:59:52 +0200

    added gen

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ from kivymd.uix.label import MDLabel
 from kivymd.uix.button import MDIconButton, MDFlatButton,MDRaisedButton
 from kivy.uix.gridlayout import GridLayout
 from kivymd.uix.toolbar import MDTopAppBar
+import random
+from kivy.properties import StringProperty
 
 
 class LoginScreen(Screen):
+    input_text = StringProperty()
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-        self.user_country_code = ""
 
         # Create App layout
         layout = BoxLayout(orientation='vertical', padding=20, spacing=20)
@@ -65,8 +67,8 @@ class LoginScreen(Screen):
     def submit_text(self, text):
         if self.validate_country_code(text):
             # The country code is valid, do something with it
-            self.user_country_code = text
-            print(self.user_country_code)
+            app = MDApp.get_running_app()
+            app.area_code = text
             self.error_label.text = ""
             self.go_to_DialerScreen()
 
@@ -74,15 +76,16 @@ class LoginScreen(Screen):
             # The country code is not valid, show an error message
             self.error_label.text = "Please enter a valid country code"
 
+    
     def go_to_DialerScreen(self, *args):
         self.manager.current = 'DialerScreen'
 
 class DialerScreen(Screen):
+    user_area_code = StringProperty()
     def __init__(self, **kwargs):
         super(DialerScreen, self).__init__(**kwargs)
-
-         # Create the toolbar and set its properties
-        toolbar = MDTopAppBar(title="Random Caller",anchor_title="left",round="40dp")
+        # Create the toolbar and set its properties
+        toolbar = MDTopAppBar(title="Random Caller",anchor_title="left")
         toolbar.pos_hint = {'top': 1}
         toolbar.size_hint = (1, None)
         toolbar.height = '48dp'
@@ -93,13 +96,14 @@ class DialerScreen(Screen):
         bottom_layout.add_widget(toolbar)
         bottom_layout.add_widget(Widget(size_hint=(1, 0.3)))
         # Add MDTextField
-        bottom_layout.add_widget(MDTextField(hint_text='Phone number', size_hint=(0.8, None), height=40,pos_hint={'center_x': .5}))
+        self.phone_number_input = MDTextField(hint_text='Phone number', size_hint=(0.8, None), height=40,pos_hint={'center_x': .5})
+        bottom_layout.add_widget(self.phone_number_input)
         # Add MDButtons
         button_layout = BoxLayout(orientation='horizontal', padding=10, spacing=10)
         # Add blank widget
         button_layout.add_widget(Widget(size_hint=(.2, .2)))
-        button_layout.add_widget(MDRaisedButton(text='Generate', on_release=self.generate_button_pressed, pos_hint={'center_y': .95,'center_x': 0},md_bg_color="green",rounded_button="True",elevation=2))
-        button_layout.add_widget(MDRaisedButton(text='Prank', on_release=self.go_to_PrankScreen, pos_hint={'center_y': .95,'center_x': .5},md_bg_color="blue",rounded_button="True",elevation=2))
+        button_layout.add_widget(MDRaisedButton(text='Generate', on_release=lambda x: self.generate_button_pressed(self.user_area_code), pos_hint={'center_y': .95,'center_x': 0},md_bg_color="green",rounded_button="True",elevation=2))
+        button_layout.add_widget(MDRaisedButton(text='Call', on_release=self.go_to_PrankScreen, pos_hint={'center_y': .95,'center_x': .5},md_bg_color="blue",rounded_button="True",elevation=2))
         button_layout.add_widget(Widget(size_hint=(.2, .2)))
         bottom_layout.add_widget(button_layout)
         # Add blank widget
@@ -107,12 +111,41 @@ class DialerScreen(Screen):
         
         self.add_widget(bottom_layout)
 
-    def generate_button_pressed(self, *args):
-        # Handle the generate button press
-        print("Generate button pressed")
+    def on_enter(self, *args):
+        app = MDApp.get_running_app()
+        self.user_area_code = app.area_code
+    import random
+
+    def generate_button_pressed(self, country_code):
+        country_code = int(country_code[1:])
+        print(country_code)
+        if len(str(country_code)) >2:
+            self.phone_number_input.text = "Invalid country code"
+            return
+        else:
+            # Replace the following with the correct rules for the country you are generating phone numbers for
+            if country_code == 1:
+                area_code = str(random.randint(201, 999))
+                prefix = random.randint(200, 999)
+                line_number = random.randint(1000, 9999)
+                self.phone_number_input.text = f"({area_code}) {prefix}-{line_number}"
+            elif country_code == 27:
+                mobile_network_operators = ["60", "61", "62", "63", "64", "65", "66", "67", "68", "69","60", "71", "72", "73", "74", "75", "76", "77", "78", "79", "81", "82", "83", "84", "85", "86", "87", "88", "89"]
+                prefix = random.choice(mobile_network_operators)
+                line_number = random.randint(1000000, 9999999)
+                ln_a = str(line_number)[:3]
+                ln_b = str(line_number)[3:7]
+                self.phone_number_input.text = f"+27 {prefix} {int(ln_a)}-{int(ln_b)}"
+
+            else:
+                # Add more elif statements for other countries here
+                self.phone_number_input.text = "Invalid country code"
+
 
     def go_to_PrankScreen(self, *args):
         self.manager.current = 'PrankScreen'
+
+
 
 class PrankScreen(Screen):
     def __init__(self, **kwargs):
@@ -127,12 +160,14 @@ class MyScreenManager(ScreenManager):
     pass
 
 class MyApp(MDApp):
+    area_code = StringProperty("")
     def build(self):
         sm = MyScreenManager()
         Window.size = (480, 720)
         sm.add_widget(LoginScreen(name='LoginScreen'))
         sm.add_widget(DialerScreen(name='DialerScreen'))
         sm.add_widget(PrankScreen(name='PrankScreen'))
+        self.root = sm
         return sm
     
 
